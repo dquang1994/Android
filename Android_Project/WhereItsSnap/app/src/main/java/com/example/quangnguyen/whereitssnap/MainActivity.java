f905cb7dc996829cf5952612b976ba7320d43c3b
Quang Nguyen <nvdq1994@gmail.com>
2018-07-31 21:22:20 +0700

    Where it's snap - Chapter 22

--- Android_Project/WhereItsSnap/app/src/main/java/com/example/quangnguyen/whereitssnap/MainActivity.java
+++ Android_Project/WhereItsSnap/app/src/main/java/com/example/quangnguyen/whereitssnap/MainActivity.java
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupDrawer() {
-        mDrawerToggle = new ActionBarDrawerToggle
+            mDrawerToggle = new ActionBarDrawerToggle
                 (this, mDrawerLayout, R.string.drawer_open,
                         R.string.drawer_close) {
 
