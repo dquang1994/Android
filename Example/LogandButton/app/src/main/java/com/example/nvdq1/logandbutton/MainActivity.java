92263991f75a2d712e982ab63129fbb186fbe09c
Quang Nguyen <nvdq1994@gmail.com>
2018-12-16 16:28:47 +0700

    Update Log and Button example

--- Example/LogandButton/app/src/main/java/com/example/nvdq1/logandbutton/MainActivity.java
+++ Example/LogandButton/app/src/main/java/com/example/nvdq1/logandbutton/MainActivity.java
@@ -4,14 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnButton1 = findViewById(R.id.btnButton1);
+        btnButton1.setOnClickListener(this);
     }
 
     public void topClick(View v){
@@ -22,5 +27,10 @@ public class MainActivity extends AppCompatActivity {
     public void bottomClick(View v){
         Toast.makeText(this, "Bottom button clicked", Toast.LENGTH_SHORT).show();
         Log.i("info", "The user clicked the bottom button");
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show();
     }
 }
