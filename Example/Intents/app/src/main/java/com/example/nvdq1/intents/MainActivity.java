05b247b11b4bf6b085dbdb21aa567f409c12a89c
Quang Nguyen <nvdq1994@gmail.com>
2020-03-25 13:54:06 +0700

    Add example for implicit intent

--- Example/Intents/app/src/main/java/com/example/nvdq1/intents/MainActivity.java
+++ Example/Intents/app/src/main/java/com/example/nvdq1/intents/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnSwitchToActivity2 = (Button) findViewById(R.id.btnSwitchToActivity2);
+        Button btnSwitchToActivity2 = findViewById(R.id.btnSwitchToActivity2);
         btnSwitchToActivity2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
 
                 // Switch to the SettingsActivity
                 startActivity(myIntent);
+            }
+        });
+
+        final EditText editText = findViewById(R.id.editText);
+        Button btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
+                startActivity(intent);
             }
         });
     }
