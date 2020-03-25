70df8331d3d47af10ee3de11df537a5ab552c7e6
Quang Nguyen <nvdq1994@gmail.com>
2020-03-25 14:22:42 +0700

    Add method 2 for simple fragment example

--- Example/Fragment/app/src/main/java/com/example/nvdq1/fragment/MainActivity.java
+++ Example/Fragment/app/src/main/java/com/example/nvdq1/fragment/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* Method 1: add by Java code */
         // Get a fragment manager
         FragmentManager fManager = getFragmentManager();
 
